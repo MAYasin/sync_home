0d3a7b1fbb15ab7d3aaa6cc2478f92d87537882b
Muhammad Aman Yasin <68651069+MAYasin@users.noreply.github.com>
2022-01-07 20:44:46 +0200

    home page layout made

--- lib/ui/views/home_view/home_view.dart
+++ lib/ui/views/home_view/home_view.dart
@@ -7,28 +7,177 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 3,
-      mainAxisSpacing: 20,
-      crossAxisSpacing: 20,
+    return Column(
       children: [
-        DeviceButton(
-          onTap: () {},
+        Column(
+          children: [
+            Row(
+              children: const [
+                Text(
+                  'Home Members',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                color: Colors.lightBlue[200],
+                elevation: 0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      const Text('Johannesburg, South Africa'),
+                      const Text('24 °C'),
+                      Card(
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        color: Colors.lightBlue[600],
+                        child: const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text(
+                            'Thunderstorm',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        DeviceButton(
-          onTap: () {},
+        const SizedBox(
+          height: 20,
         ),
-        DeviceButton(
-          onTap: () {},
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Text(
+              'Devices',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'See All',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
-        DeviceButton(
-          onTap: () {},
+        const SizedBox(
+          height: 20,
         ),
-        DeviceButton(
-          onTap: () {},
+        Row(
+          children: [
+            CircleAvatar(
+              radius: 30,
+              backgroundColor: Colors.pink,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.wb_sunny_outlined,
+                ),
+              ),
+            ),
+            CircleAvatar(
+              radius: 30,
+              backgroundColor: Colors.pink,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.wb_sunny_outlined,
+                ),
+              ),
+            ),
+            CircleAvatar(
+              radius: 30,
+              backgroundColor: Colors.pink,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.wb_sunny_outlined,
+                ),
+              ),
+            ),
+            CircleAvatar(
+              radius: 30,
+              backgroundColor: Colors.pink,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.wb_sunny_outlined,
+                ),
+              ),
+            ),
+          ],
         ),
-        DeviceButton(
-          onTap: () {},
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Text(
+              'Rooms',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        GridView.count(
+          shrinkWrap: true,
+          crossAxisCount: 3,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+          children: [
+            DeviceButton(
+              onTap: () {},
+            ),
+            DeviceButton(
+              onTap: () {},
+            ),
+            DeviceButton(
+              onTap: () {},
+            ),
+            DeviceButton(
+              onTap: () {},
+            ),
+            DeviceButton(
+              onTap: () {},
+            ),
+            DeviceButton(
+              onTap: () {},
+            ),
+          ],
         ),
       ],
     );

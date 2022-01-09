c5bd7ee1d4072d919e08e26b18a4e09f102aa85b
Muhammad Aman Yasin <68651069+MAYasin@users.noreply.github.com>
2022-01-09 22:44:34 +0200

    weather card

--- lib/ui/components/weather_card/weather_card.dart
+++ lib/ui/components/weather_card/weather_card.dart
@@ -21,31 +21,122 @@ class WeatherCard extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10),
         ),
-        color: Colors.lightBlue[200],
+        color: Colors.grey[200],
         elevation: 0,
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Row(
             children: [
-              const Text('Johannesburg, South Africa'),
-              const Text('24 °C'),
-              Card(
-                margin: const EdgeInsets.all(0),
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40),
-                ),
-                color: Colors.lightBlue[600],
-                child: const Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text(
-                    'Thunderstorm',
-                    style: TextStyle(
-                      color: Colors.white,
+              Image.asset(
+                'assets/icon/weather/wind.png',
+                width: 120,
+                height: 120,
+                scale: 0.5,
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: const [
+                      Icon(
+                        Icons.location_pin,
+                      ),
+                      SizedBox(
+                        width: 2,
+                      ),
+                      Text(
+                        'Johannesburg, South Africa',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        '24',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '°C',
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(width: 40),
+                      Column(
+                        children: [
+                          Text(
+                            'Avg. Inside',
+                            style: TextStyle(
+                              color: Colors.grey[900],
+                              fontSize: 12,
+                            ),
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                '19',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '°C',
+                                style: TextStyle(
+                                  color: Colors.grey[700],
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Card(
+                    margin: const EdgeInsets.all(0),
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    color: Colors.lightBlue[600],
+                    child: const Padding(
+                      padding: EdgeInsets.only(
+                        left: 20,
+                        top: 12,
+                        right: 20,
+                        bottom: 12,
+                      ),
+                      child: Text(
+                        'Thunderstorm',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
             ],
           ),

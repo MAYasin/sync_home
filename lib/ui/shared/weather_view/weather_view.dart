fdf64bf0f3ddc5e93c259c083084dec4037dd76c
Muhammad Aman Yasin <68651069+MAYasin@users.noreply.github.com>
2022-01-10 18:12:21 +0200

    weather feature added

--- lib/ui/shared/weather_view/weather_view.dart
+++ lib/ui/shared/weather_view/weather_view.dart
@@ -1,14 +1,184 @@
 import 'package:flutter/material.dart';
 import 'package:sync_home/ui/shared/base_view.dart';
+import 'package:weather/weather.dart';
 
-class WeatherView extends StatelessWidget {
+enum AppState { NOT_DOWNLOADED, DOWNLOADING, FINISHED_DOWNLOADING }
+
+class WeatherView extends StatefulWidget {
   const WeatherView({Key? key}) : super(key: key);
 
   @override
+  State<WeatherView> createState() => _WeatherViewState();
+}
+
+class _WeatherViewState extends State<WeatherView> {
+  String key = '856822fd8e22db5e1ba48c0e7d69844a';
+  late WeatherFactory ws;
+  List<Weather> _data = [];
+  AppState _state = AppState.NOT_DOWNLOADED;
+  double? lat, lon;
+
+  @override
+  void initState() {
+    super.initState();
+    ws = new WeatherFactory(key);
+  }
+
+  void queryForecast() async {
+    /// Removes keyboard
+    FocusScope.of(context).requestFocus(FocusNode());
+    setState(() {
+      _state = AppState.DOWNLOADING;
+    });
+
+    List<Weather> forecasts = await ws.fiveDayForecastByLocation(lat!, lon!);
+    setState(() {
+      _data = forecasts;
+      _state = AppState.FINISHED_DOWNLOADING;
+    });
+  }
+
+  void queryWeather() async {
+    /// Removes keyboard
+    FocusScope.of(context).requestFocus(FocusNode());
+
+    setState(() {
+      _state = AppState.DOWNLOADING;
+    });
+
+    Weather weather = await ws.currentWeatherByLocation(lat!, lon!);
+    setState(() {
+      _data = [weather];
+      _state = AppState.FINISHED_DOWNLOADING;
+    });
+  }
+
+  Widget contentFinishedDownload() {
+    return Center(
+      child: ListView.separated(
+        shrinkWrap: true,
+        itemCount: _data.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_data[index].toString()),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const Divider();
+        },
+      ),
+    );
+  }
+
+  Widget contentDownloading() {
+    return Container(
+      margin: const EdgeInsets.all(25),
+      child: Column(children: [
+        const Text(
+          'Fetching Weather...',
+          style: TextStyle(fontSize: 20),
+        ),
+        Container(
+            margin: const EdgeInsets.only(top: 50),
+            child:
+                const Center(child: CircularProgressIndicator(strokeWidth: 10)))
+      ]),
+    );
+  }
+
+  Widget contentNotDownloaded() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const <Widget>[
+          Text(
+            'Press the button to download the Weather forecast',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _resultView() => _state == AppState.FINISHED_DOWNLOADING
+      ? contentFinishedDownload()
+      : _state == AppState.DOWNLOADING
+          ? contentDownloading()
+          : contentNotDownloaded();
+
+  void _saveLat(String input) {
+    lat = double.tryParse(input);
+    print(lat);
+  }
+
+  void _saveLon(String input) {
+    lon = double.tryParse(input);
+    print(lon);
+  }
+
+  Widget _coordinateInputs() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+              margin: const EdgeInsets.all(5),
+              child: TextField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Enter latitude'),
+                  keyboardType: TextInputType.number,
+                  onChanged: _saveLat,
+                  onSubmitted: _saveLat)),
+        ),
+        Expanded(
+            child: Container(
+                margin: const EdgeInsets.all(5),
+                child: TextField(
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter longitude'),
+                    keyboardType: TextInputType.number,
+                    onChanged: _saveLon,
+                    onSubmitted: _saveLon)))
+      ],
+    );
+  }
+
+  Widget _buttons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.all(5),
+          child: TextButton(
+            child: const Text(
+              'Fetch weather',
+              style: const TextStyle(color: Colors.white),
+            ),
+            onPressed: queryWeather,
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue)),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(5),
+          child: TextButton(
+            child: const Text(
+              'Fetch forecast',
+              style: const TextStyle(color: Colors.white),
+            ),
+            onPressed: queryForecast,
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue)),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const BaseView(
+    return BaseView(
       centerTitle: true,
-      title: Text(
+      title: const Text(
         'Weather',
         style: TextStyle(
           color: Colors.black,
@@ -16,8 +186,20 @@ class WeatherView extends StatelessWidget {
           fontWeight: FontWeight.bold,
         ),
       ),
-      child: Text(
-        'weather',
+      child: Column(
+        children: <Widget>[
+          _coordinateInputs(),
+          _buttons(),
+          const Text(
+            'Output:',
+            style: TextStyle(fontSize: 20),
+          ),
+          const Divider(
+            height: 20.0,
+            thickness: 2.0,
+          ),
+          _resultView(),
+        ],
       ),
     );
   }

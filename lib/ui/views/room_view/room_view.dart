74596c1db9c69e4c847738be011551adfab9d76b
Muhammad Aman Yasin <68651069+MAYasin@users.noreply.github.com>
2022-01-15 14:00:32 +0200

    partial room button

--- lib/ui/views/room_view/room_view.dart
+++ lib/ui/views/room_view/room_view.dart
@@ -1,14 +1,80 @@
 import 'package:flutter/material.dart';
 
-import '../../shared/base_view.dart';
-
 class RoomView extends StatelessWidget {
   const RoomView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('Room'),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 180,
+          width: 180,
+          decoration: BoxDecoration(
+            color: Colors.amber[200],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: Column(
+                  children: const [
+                    Icon(
+                      Icons.bedroom_parent_outlined,
+                      size: 80,
+                    ),
+                    Text(
+                      'Room',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                highlightColor: Colors.amber,
+                child: Column(
+                  children: [
+                    const Divider(
+                      thickness: 1,
+                      color: Colors.black,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.wb_sunny_outlined,
+                          size: 25,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Lights',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }

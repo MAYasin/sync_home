e486cde51aaaa74e7ccbf7bc5d8f1558d9ad1ad2
Muhammad Aman Yasin <68651069+MAYasin@users.noreply.github.com>
2022-07-24 21:00:42 +0200

    rooms section

--- lib/ui/components/button/buttons.dart
+++ lib/ui/components/button/buttons.dart
@@ -102,3 +102,68 @@ class _DeviceButtonState extends State<DeviceButton> {
     );
   }
 }
+
+class RoomButton extends StatefulWidget {
+  const RoomButton({
+    Key? key,
+    required this.onTap,
+    required this.name,
+    required this.icon,
+  }) : super(key: key);
+
+  final Function onTap;
+  final String name;
+  final IconData icon;
+
+  @override
+  State<RoomButton> createState() => _RoomButtonState();
+}
+
+class _RoomButtonState extends State<RoomButton> {
+  Color textColorLight = const Color.fromARGB(255, 145, 145, 145);
+  Color borderColorLight = const Color.fromARGB(242, 242, 242, 255);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        widget.onTap;
+        HapticFeedback.vibrate();
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: textColorLight, width: 2),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(18),
+              child: Column(
+                children: [
+                  Icon(
+                    widget.icon,
+                    size: 50,
+                    color: textColorLight,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            widget.name,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.normal,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/views/room_view/room_view.dart
+++ lib/ui/views/room_view/room_view.dart
@@ -1,78 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:sync_home/ui/components/button/buttons.dart';
 
 class RoomView extends StatelessWidget {
   const RoomView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return GridView.count(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      crossAxisCount:
+          MediaQuery.of(context).orientation == Orientation.portrait ? 3 : 4,
+      mainAxisSpacing: 20,
+      crossAxisSpacing: 20,
       children: [
-        Container(
-          height: 180,
-          width: 180,
-          decoration: BoxDecoration(
-            color: Colors.amber[200],
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: Column(
-                  children: const [
-                    Icon(
-                      Icons.bedroom_parent_outlined,
-                      size: 80,
-                    ),
-                    Text(
-                      'Room',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              InkWell(
-                onTap: () {},
-                highlightColor: Colors.amber,
-                child: Column(
-                  children: [
-                    const Divider(
-                      thickness: 1,
-                      color: Colors.black,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          Icons.wb_sunny_outlined,
-                          size: 25,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          'Lights',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+        RoomButton(
+          onTap: () {},
+          name: 'Bedroom',
+          icon: Icons.bedroom_parent_outlined,
+        ),
+        RoomButton(
+          onTap: () {},
+          name: 'Bedroom',
+          icon: Icons.bedroom_child_outlined,
+        ),
+        RoomButton(
+          onTap: () {},
+          name: 'Kitchen',
+          icon: Icons.kitchen,
+        ),
+        RoomButton(
+          onTap: () {},
+          name: 'Living Room',
+          icon: Icons.tv,
+        ),
+        RoomButton(
+          onTap: () {},
+          name: 'Dining Room',
+          icon: Icons.table_restaurant,
         ),
       ],
     );
